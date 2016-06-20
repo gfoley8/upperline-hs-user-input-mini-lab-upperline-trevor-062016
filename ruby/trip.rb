70c0ceac657b34e45ed91690827087d079e58cbd
Grace Foley <gfoley18@spenceschool.org>
2016-06-20 16:20:11 +0000

    Done.

--- ruby/trip.rb
+++ ruby/trip.rb
@@ -1,1 +1,10 @@
-#Code here!
+puts"Where would you like to stay?"
+hotel=gets.chomp
+puts"What is one place you would like to go?"
+place1=gets.chomp
+puts"What is another place you would like to go?"
+place2=gets.chomp
+puts"How many days would you like to stay?"
+lengthStay=gets.chomp
+
+puts"You will stay in "+hotel+" for "+lengthStay+" days"+" and go to "+place1+" and "+place2+". I hope you have fun!"
